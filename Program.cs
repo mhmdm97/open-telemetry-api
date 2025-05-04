ffdfe72f24e5f7e4467a615a2ab270477dfb9728
Mohamad Mortada <mhmdmor97@gmail.com>
2025-05-05 02:10:02 +0300

    added telemetry and scalar

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(x => x.AddOtlpExporter(a => 
+{
+    a.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/logs");
+    a.Protocol = OtlpExportProtocol.HttpProtobuf;
+    a.Headers = "X-Seq-ApiKey=bQ8iaI9kcKoQJGivHv4q";
+}));
+
+// Add services to the container.]
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -10,6 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -19,23 +31,26 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weather", (string city, int days, ILogger<Program> logger) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast =  Enumerable.Range(1, days).Select(index =>
         new WeatherForecast
         (
+            city,
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+    logger.LogInformation("Weather forecast for {City} for {Days} days", city, days);
+    logger.LogInformation("Weather forecast: {Forecast}", forecast);
     return forecast;
 })
 .WithName("GetWeatherForecast");
 
-app.Run();
+app.Run(); 
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record WeatherForecast(string City, DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
